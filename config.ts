36a59086398522f41ac8709a39b7ec3b571f999e
Simon Pope <pope.audivit@gmail.com>
2023-10-10 13:45:52 +1300

    Uses cache to check if star has been called

--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL || "";
 export const DISCORD_UPDATE_WEBHOOK_URL = process.env.DISCORD_UPDATE_WEBHOOK_URL || "";
+export const CACHE_TIMEOUT = process.env.CACHE_TIMEOUT ? parseInt(process.env.CACHE_TIMEOUT) : 7200;
